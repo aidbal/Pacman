a1b209dba01e630d6c0e5eb16e29329bdd9452f3
Žygimantas Skinkys <skinkyszygimantas@gmail.com>
2017-11-07 08:43:17 +0200

    progress

---
+++ Pacman/Pacman/Pacman/Cannon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pacman
+{
+    class Cannon : WeaponDecorator
+    {
+    }
+}
---
+++ Pacman/Pacman/Pacman/SpeedTrap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pacman
+{
+    class SpeedTrap : WeaponDecorator
+    {
+        
+    }
+}
